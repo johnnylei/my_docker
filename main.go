9814a6c7eac4a083d6524c8b8e2ec025280fbec6
johnny <leiyuqing_jing@163.com>
2019-10-05 00:18:28 +0800

    update

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/johnnylei/my_docker/cgroups"
 	"github.com/johnnylei/my_docker/namespace"
 )
 
 func main() {
 	namespace.Segregate()
-	//cgroups.Run()
+	cgroups.Run()
 }
