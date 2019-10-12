84d3941c60162d99ab77a463254cf4b4ed62f6e8
leiyuqing02 <leiqyuing_jing@163.com>
2019-10-12 14:04:58 +0800

    update

---
+++ container/commit.go
@@ -0,0 +1,32 @@
+package container
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"os"
+	"os/exec"
+	"path"
+)
+
+const MountRoot  = "/tmp/mnt"
+func Commit(context *cli.Context) error  {
+	name := context.String("name")
+	if name == "" {
+		return fmt.Errorf("container name should not be empty")
+	}
+
+	fileSystemPath := path.Join(MountRoot, name)
+	if _, err := os.Stat(fileSystemPath); err != nil {
+		return fmt.Errorf("%s error, error message:%s", fileSystemPath, err.Error())
+	}
+
+	imageTarName := fmt.Sprintf("%s.tar", name)
+	cmd := exec.Command("tar", "-cf", imageTarName, fileSystemPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("tar failed, tar is:%s; file system path:%s", imageTarName, fileSystemPath)
+	}
+
+	return nil
+}
