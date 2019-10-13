3feaddec7819ed293333c7bdec013069247133d3
johnny <leiyuqing_jing@163.com>
2019-10-13 21:08:46 +0800

    update

--- container/run.go
+++ container/run.go
@@ -1,7 +1,6 @@
 package container
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/johnnylei/my_docker/subsystem"
 	"github.com/johnnylei/my_docker/util"
@@ -9,7 +8,6 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path"
 	"strings"
 	"syscall"
 	"time"
@@ -99,6 +97,10 @@ func Run(c *cli.Context) error  {
 	if tty {
 		if err := cmd.Wait(); err != nil {
 			return err
+		}
+
+		if err := containerInformation.Destroy(); err != nil {
+			return  err
 		}
 	}
 
@@ -254,54 +256,6 @@ func initContainerVolume(path string, c *cli.Context) error  {
 	mountOptions := fmt.Sprintf("dirs=%s", mounts[0])
 	if err := exec.Command("mount", "-t", "aufs", "-o", mountOptions, "none", destinationMount).Run(); err != nil {
 		return fmt.Errorf("init Cointainer volumen failed; mount failed; source:%s; destination:%s", mounts[0], destinationMount)
-	}
-
-	return nil
-}
-
-type ContainerInformation struct {
-	Pid int `json:"pid"`
-	Id string `json:"id"`
-	Name string `json:"name"`
-	InitCommand string `json:"init_command"`
-	Status string `json:"status"`
-	CreatedTime string `json:"created_time"`
-}
-
-func (information *ContainerInformation) Record() error  {
-	if _, err := os.Stat(DefaultContainerInformationLocation); os.IsNotExist(err) {
-		if err := os.Mkdir(DefaultContainerInformationLocation, 077); err != nil {
-			return fmt.Errorf("mkdir %s failed, err:%s", DefaultContainerInformationLocation, err.Error())
-		}
-	}
-
-	BasePath := path.Join(DefaultContainerInformationLocation, information.Name)
-	if _, err := os.Stat(BasePath); os.IsNotExist(err) {
-		if err := os.Mkdir(BasePath, 0777); err != nil {
-			return fmt.Errorf("mkdir %s failed, err:%s", BasePath, err.Error())
-		}
-	}
-
-	InformationFile := path.Join(BasePath, InformationFileName)
-	if _, err := os.Stat(InformationFile); os.IsNotExist(err) {
-		if _, err := os.Create(InformationFile); err != nil {
-			return fmt.Errorf("create %s failed, err:%s", InformationFile, err.Error())
-		}
-	}
-
-	InformationFileFd, err := os.OpenFile(InformationFile, os.O_WRONLY, 0644)
-	if err != nil {
-		return fmt.Errorf("open %s failed, error:%s", InformationFile, err.Error())
-	}
-
-	informationBytes, err := json.Marshal(information)
-	if err != nil {
-		return fmt.Errorf("json encode failed; err:%s", err.Error())
-	}
-
-	informationJson := string(informationBytes)
-	if _, err := InformationFileFd.WriteString(informationJson); err != nil {
-		return fmt.Errorf("wirte information failed; file:%s, information:%s, error:%s", InformationFile, informationJson, err.Error())
 	}
 
 	return nil
